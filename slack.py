163a3e8cfd6fb491b03395055551fb641914aa5f
Max Remetz <max@remetz.com>
2019-02-17 04:24:59 -0500

    thread formatting

--- slack.py
+++ slack.py
@@ -41,21 +41,23 @@ class SlackBot:
 
 
     def handle_message(self, message):
+      sender = message.get('username', message['user'])
       if 'reactions' in message:
           for reaction in message['reactions']:
             for user in reaction['users']:
               rusername = self.userlist.get(user)
               reaction['users'].remove(user)
               reaction['users'].append(rusername)
-
+      
       parents = {}
       if 'parent_user_id' not in message:
-        parents[message['ts']] = list(message['text'])
+        parents[message['ts']] = list({'sender': sender, 'message': message['text']})
       else:
         message['thread'] = parents[message['thread_ts']]
-        parents[message['thread_ts']].append(message['text'])
+        parents[message['thread_ts']].append({'sender': sender, 'message': message['text']})
 
       m = {
+        'sender': sender,
         'type': 'MSG',
         'channel': self.channels[channel],
         'text': message['text'],
@@ -63,13 +65,7 @@ class SlackBot:
         'thread': message.get('thread', [])
       }
 
-      if "user" in message:
-        m['sender'] = self.userlist[message.get('user')]
-        self.to_discord.put(m)
-        
-      elif "username" in message and message.get('username') != self._username:
-        m['sender'] = message.get('username')
-        self.to_discord.put(m)
+      self.to_discord.put(m)
 
 
     def channel_listener(self, channel):
