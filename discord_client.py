45a4095ec9f4061a220a816213ba0d2c4e742bf7
Connor Zapfel <czapfel@stevens.edu>
2019-02-16 21:54:09 -0500

    Auto-generating channels

--- discord_client.py
+++ discord_client.py
@@ -1,6 +1,7 @@
 import discord
 import threading
 import asyncio
+import json
 
 class DiscordClient(discord.Client):
 
@@ -10,6 +11,9 @@ class DiscordClient(discord.Client):
         # Store the queues
         self.to_slack = to_slack
         self.from_slack = from_slack
+        # Create instance vars
+        self.channels = dict()
+        self.server = None
 
     @asyncio.coroutine
     def on_message(self, message):
@@ -24,12 +28,23 @@ class DiscordClient(discord.Client):
         print('Logged in as')
         print(self.user.name)
         print(self.user.id)
+        print([x for x in self.servers])
+        self.server = [x for x in self.servers][0]
         print('------')
-        # Get the channels
-        channel = [x for x in self.get_all_channels()][1]
         while True:
             # Wait for a message
             msg = self.from_slack.get(block=True)
-            print(f"Received from slack: {msg}")
-            # Forward it to the discord server
-            yield from self.send_message(channel, msg)
+            # Parse to JSON
+            jmsg = json.loads(msg)
+            # Check the message type
+            if jmsg["type"] == "MSG":
+                print(f"Received from slack: {msg}")
+                # Forward it to the discord server
+                yield from self.send_message(self.channels[jmsg["channel"]], f"{jmsg["sender"]}: {jmsg["text"]}")
+            elif jmsg["type"] == "CONF":
+                # Create all the channels
+                for ch_name in jmsg["channels"]:
+                    yield from self.create_channel(self.server, ch_name)
+                # Store then channel mapping
+                for ch in [c for c in self.server.get_all_channels()]:
+                    self.channels[ch.name] = ch
